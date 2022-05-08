d31594ecba1075771d96bdfe20566a6ec5bc6641
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2022-05-08 21:55:25 +0900

    0.5.5

---
+++ app/xcode/Sources/VCamUI/Extensions/Array+.swift
@@ -0,0 +1,23 @@
+//
+//  Array+.swift
+//  
+//
+//  Created by Tatsuya Tanaka on 2022/05/07.
+//
+
+import Foundation
+
+public extension Array where Element: Identifiable {
+    func index(ofId id: Element.ID) -> Int? {
+        firstIndex { $0.id == id }
+    }
+
+    func find(byId id: Element.ID) -> Element? {
+        first { $0.id == id }
+    }
+
+    mutating func remove(byId id: Element.ID) {
+        self = filter { $0.id != id }
+    }
+}
+
