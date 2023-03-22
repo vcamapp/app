61521b1f705d51c1372087f81239f9e6d2188ea0
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2023-03-23 01:12:51 +0900

    Fix test

--- app/xcode/Sources/VCamData/UserDefaultsKey.swift
+++ app/xcode/Sources/VCamData/UserDefaultsKey.swift
@@ -14,4 +14,5 @@ public extension UserDefaults.Key {
     static var skipThisVersion: Key<Version> { .init("vc_skip_version", default: "0.0.0") }
     static var useVowelEstimation: Key<Bool> { .init("vc_use_vowel_estimation", default: false) }
     static var locale: Key<String> { .init("vc_locale", default: "") }
+    static var pluginVersion: Key<String> { .init("vc_plugin_ver", default: "") }
 }
