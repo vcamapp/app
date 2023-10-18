e489391ce51cea039b6fcffcc40bd488e277a22d
tattn (Tatsuya Tanaka) <tanakasan2525@gmail.com>
2023-10-19 00:09:21 +0900

    Add an initial UI test

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-all: test-package build-ui-preview
+all: test-package build-ui-preview test-ui-preview
 
 test-package:
 	cd app/xcode && swift test
 
 build-ui-preview:
 	cd app/xcode/App && xcodebuild -project VCam.xcodeproj -scheme VCamUIPreview -derivedDataPath /tmp/build clean build
+
+test-ui-preview:
+	cd app/xcode/App && xcodebuild -project VCam.xcodeproj -scheme VCamUIPreviewUITests -derivedDataPath /tmp/build test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
 
---
+++ app/xcode/App/VCamUIPreviewUITests/VCamUIPreviewUITests.swift
@@ -0,0 +1,36 @@
+//
+//  VCamUIPreviewUITests.swift
+//  VCamUIPreviewUITests
+//
+//  Created by Tatsuya Tanaka on 2023/10/18.
+//
+
+import XCTest
+import VCamLocalization
+
+final class VCamUIPreviewUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        LocalizationEnvironment.currentLocale = { "en_US" }
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testLaunchApp() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        XCTContext.runActivity(named: "Close the alert about the virtual camera if needed") { _ in
+            if app.staticTexts[L10n.installVirtualCamera.text].exists {
+                app.buttons["OK"].click()
+                app.buttons["OK"].click()
+            }
+        }
+
+        XCTContext.runActivity(named: "Check VCamUI") { _ in
+            XCTAssertTrue(app.buttons["Main"].exists)
+        }
+    }
+}
